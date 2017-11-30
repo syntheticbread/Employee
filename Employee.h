31a0c019fe15ecf0a2cd20fcecd63c5145af1400
syntheticbread <31804707+syntheticbread@users.noreply.github.com>
2017-11-29 19:52:21 -0500

    Add files via upload
    Update

--- Employee.h
+++ Employee.h
@@ -8,31 +8,32 @@ using namespace std;
 
 class INFO {
 	public:
+		string Name;
+		string Adress;
+		int ID, Age, Salary;
 		
 		void Employee() {
-			string Name;
-			string Adress;
-			int ID, Age, Salary;
+			
 			cin.ignore();
 			cout << "Please enter employee name \n";
 			getline(cin, Name);
-			
-			
-			
+
+
+
 			cout << "please enter employee address \n";
 			getline(cin, Adress);
-			
-			
+
+
 			cout << "Please enter employee age \n";
 			cin >> Age;
-			
+
 			cout << "Please enter employee salary \n";
 			cin >> Salary;
-		
+
 			cout << "Please enter employee Id \n";
 			cin >> ID;
-			
-			
+
+
 			ofstream myfile;
 			myfile.open("Employee Data.txt", ios::out | ios::app);
 			myfile << "Name: " << Name << endl;
@@ -42,11 +43,30 @@ class INFO {
 			myfile << "ID: " << ID << endl;
 			myfile << endl;
 			myfile.close();
-			
-			
-
-
 		}
+			
+		void search() {
+			string   line;
+			ifstream file("Employee Data.txt");
+			while (getline(file, line))
+			{
+				
+			
+				cout << line << "\n";
+				
+
+			}
+
+			
+						
+			
+		}
+
+
+													  
+		
+
+		
 
 
 };
--- Source.cpp
+++ Source.cpp
@@ -1,43 +1,56 @@
 #include "Employee.h"
-#include <stdlib.h>
+#include <cstdlib>
 #include <windows.h>
+#include "Login.h"
+
 
 void menu();
 
 void menu() {
 	cout << "\t \t \t \t \t \t \t DATA inc. \n";
 	cout << "1: CREATE \n";
-	cout << "2: EXIT \n";
+	cout << "2: SEARCH RECORDS \n";
+	cout << "3: EXIT \n";
+	
 }
-int main(){
+int main() {
 	int options = 0;
-	bool active = true;
-	while (active) {
-		menu();
-		cin >> options;
-		if (options == 1) {
+	LOGIN user;
+	user.Login_window();
+	Sleep(1500);
+	system("cls");
+
+			menu();
+			cin >> options;
+			if (options == 1) {
 
 
-			INFO option;
-			option.Employee();
+				INFO option;
+				option.Employee();
 
 
 
-		}
+			}
 
-		if (options == 2) {
-			cout << "Exiting.....";
-			Sleep(2000);
-			bool active = false;
-			return 0;
-		}
+			if (options == 3) {
+				cout << "Exiting.....";
+				Sleep(2000);
+				bool active = false;
+				return 0;
+			}
+
+
+			if (options == 2) {
+				INFO options2;
+				options2.search();
+			}
+
+
+
+
+
+	
 	}
 	
-		
 
 
-		
-	
-	system("pause");
-
-}
